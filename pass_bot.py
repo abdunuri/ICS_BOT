c68621b3dea1e259766e46187628824b66655bd8
Abdulaziz <abdulaziznuri495@gmail.com>
2025-05-17 11:12:37 +0300

    initial commit

--- pass_bot.py
+++ pass_bot.py
@@ -938,7 +938,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     try:
         playwright = await async_playwright().start()
         await status_msg.edit_text("⚡Launching browser...")
-        browser = await playwright.chromium.launch(headless=False)
+        browser = await playwright.chromium.launch(headless=True)
         browser_context = await browser.new_context()
         page = await browser_context.new_page()
         await page.goto("https://www.ethiopianpassportservices.gov.et/request-appointment", wait_until="domcontentloaded")
